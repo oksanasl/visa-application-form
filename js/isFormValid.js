12db3eac67a4ad7c8092e9f7a424f5db3f8e3027
oksinlove <a.smile.of.fortune@gmail.com>
2017-07-16 00:21:12 +0300

    linkRadioWithInput, euIsChecked logic functions added

---
+++ js/isFormValid.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+// function requiredFieldsNotEmpty(){
+//     $('input').prop('required', true);
+//     if($('[required]').val() != ''){
+//         $('input[type="submit"]').attr("disabled", false);
+//     }
+// }
+
+/**
+ * Created by Окси on 16.07.2017.
+ */
--- js/logic.js
+++ js/logic.js
@@ -5,10 +5,28 @@
 function linkRadioWithInput(){
     $("#other-passport-type").focus();
     $("#other-passport-type").change(function(){
-        //$("#other-passport").val("My value");
         $("#other-passport").val($(this).val());
         alert($("#other-passport").val());
     });
+}
+
+function euIsChecked(){
+    if ($('#euna:checked').length > 0) {
+        $('input[name="eufam"]').attr("disabled", true);
+    }
+    else{
+        $('input[name="eufam"]').attr("disabled", false);
+    }
+};
+
+
+
+function submitForm(){
+    if (true || isFormValid()) {
+        $('form').submit();
+}
+
+
 }
 /**
  * Created by Окси on 14.07.2017.
