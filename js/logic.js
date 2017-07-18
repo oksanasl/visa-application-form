2d80126f4fe655a1952542a9da9d0c6bb0ab46e6
oksinlove <a.smile.of.fortune@gmail.com>
2017-07-18 17:20:35 +0300

    insertName, setMaxDate functions added

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,12 @@
 // $(document).ready(function(){
-//     alert("I AM LINKED");
+//     alert(m + "/" + d + "/" + y);
 // });
 
 function linkRadioWithInput(){
     $("#other-passport-type").focus();
     $("#other-passport-type").change(function(){
         $("#other-passport").val($(this).val());
-        alert($("#other-passport").val());
+        //alert($("#other-passport").val());
     });
 }
 
@@ -18,6 +18,38 @@ function euIsChecked(){
         $('input[name="eufam"]').attr("disabled", false);
     }
 };
+
+
+function declarationIsChecked(){
+
+}
+
+function insertName(){
+    var userName = document.getElementById("username").value;
+    var userSurname = document.getElementById("surname").value;
+    var nameInDeclaration = document.getElementById("name-insertion");
+    nameInDeclaration.append(', ' + userName + ' ' + userSurname + ',');
+}
+
+function setMaxDate(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if (dd < 10){
+        dd = '0' + dd;
+    }
+    if (mm < 10){
+        mm = '0' + mm;
+    }
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("dateofbirth").setAttribute("max", today);
+
+    //document.getElementsByClassName("setmax").setAttribute("max", today);
+
+    //alert(today);
+}
+
 
 
 
